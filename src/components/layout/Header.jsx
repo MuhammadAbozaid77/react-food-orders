346990d17d1c8fd4c347380b00e315a841d76a96
YoussefRefai <React.2@smartgate-egypt.com>
2025-05-07 11:27:27 +0300

    some details

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,30 +1,40 @@
 import Navbar from "./Navbar";
 import Logo from "./Logo";
-
 import { FaHeart, FaShoppingCart, FaUserAlt } from "react-icons/fa";
 
 export default function Header() {
+  // const { cart } = useSelector((state) => state.cart);
+
+  // console.log(cart);
+
   return (
     <div className="flex w-[100%] justify-between px-[50px] items-center bg-Color200  h-[80px]">
       <Logo type={""} />
       <Navbar />
       <div className="flex justify-between items-center gap-2">
         <div>
-          <div className="flex justify-center items-center cursor-pointer">
+          <div className="flex justify-center items-center cursor-pointer relative">
+            <div className="absolute -top-[15px] -right-[5px] w-[25px] h-[25px] rounded-full font-bold flex justify-center items-center bg-gray-500 text-white">
+              {/* {cart?.length} */}
+            </div>
+
             <div className="bg-logoColor text-Color100 rounded-lg p-[8px]">
               <FaHeart size={22} />
             </div>
           </div>
         </div>
         <div>
-          <div className="flex justify-center items-center cursor-pointer">
+          <div className="flex justify-center items-center cursor-pointer relative">
+            <div className="absolute -top-[15px] -right-[5px] w-[25px] h-[25px] rounded-full font-bold flex justify-center items-center bg-gray-500 text-white">
+              {/* {cart?.length} */}
+            </div>
             <div className="bg-logoColor text-Color100 rounded-lg p-[8px]">
               <FaShoppingCart size={22} />
             </div>
           </div>
         </div>
         <div>
-          <div className="flex justify-center items-center cursor-pointer">
+          <div className="flex justify-center items-center  cursor-pointer">
             <div className="bg-logoColor text-Color100 rounded-lg p-[8px]">
               <FaUserAlt size={22} />
             </div>
---
+++ src/components/ui/AdvertsimsntCard.jsx
@@ -0,0 +1,8 @@
+export default function AdvertsimsntCard() {
+  return (
+    <div className="bg-white border h-[400px]">
+      <div>ggggg</div>
+      <div className="box"></div>
+    </div>
+  );
+}
